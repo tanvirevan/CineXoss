9b9a70f47be87ba0b3e6ecfc2544ca2fecefc625
Tanvir Islam <pro.tanvirevan@gmail.com>
2024-10-22 11:12:08 +0600

    Start and Done Header

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import Moon from "./assets/icons/moon.svg";
+// import Sun from "/assets/icons/sun";
+import Logo from "./assets/logo.svg";
+import Ring from "./assets/ring.svg";
+import ShoppingCart from "./assets/shopping-cart.svg";
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="container flex items-center justify-between space-x-10 py-6">
+              <a href="index.html">
+                  <img src={Logo} width="70" height="20" alt="logo" />
+              </a>
+
+              <ul className="flex items-center space-x-5">
+                <li>
+                  <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"href="#">
+                    <img src={Ring} width="24" height="24" alt="ring" />
+                  </a>
+                </li>
+                <li>
+                  <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                    <img src={Moon} width="24" height="24" alt="theme-icon" />
+                  </a>
+                </li>
+                <li>
+                  <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                    <img
+                      src={ShoppingCart}
+                      width="24"
+                      height="24"
+                      alt="cart"
+                    />
+                  </a>
+                </li>
+              </ul>
+            </nav>
+        </header>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: 
+    {
+      container:
+        {
+          center: true,
+          padding: "1.25"
+        },
+      colors: 
+        {
+          primary: "#00D991"
+        }
+    },
+};
+export const plugins = [];
